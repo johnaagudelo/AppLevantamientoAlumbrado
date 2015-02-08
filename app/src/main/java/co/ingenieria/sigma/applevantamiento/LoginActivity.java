68c01dead7bcc24f29f8df3e9ff8c33f733acbfa
John Agudelo <johnaagudelo@gmail.com>
2015-02-08 17:22:39 -0500

    validacion de inicio de sesion login sharedPreferences

--- app/src/main/java/co/ingenieria/sigma/applevantamiento/LoginActivity.java
+++ app/src/main/java/co/ingenieria/sigma/applevantamiento/LoginActivity.java
@@ -1,18 +1,59 @@
 package co.ingenieria.sigma.applevantamiento;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import co.ingenieria.sigma.models.clsLogin;
 
 
 public class LoginActivity extends ActionBarActivity {
+
+    private static String usuario = "johnaagudelo";
+    private static String pass = "123";
+    EditText edi_usuario;
+    EditText edi_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        edi_usuario = (EditText) findViewById(R.id.usu);
+        edi_pass = (EditText) findViewById(R.id.clave);
     }
+
+    public void login(View view){
+        if(edi_usuario.getText().equals("")) {
+            if(edi_pass.getText().equals("")) {
+                if (edi_usuario.getText().equals(usuario) && edi_pass.getText().equals(pass)) {
+                    SharedPreferences login = getSharedPreferences("Login_Usuario", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = login.edit();
+                    editor.putString("usuario", edi_usuario.getText().toString());
+                    editor.putString("clave", edi_pass.getText().toString());
+                } else {
+                    Toast.makeText(this, "Usuario Incorrecto", Toast.LENGTH_LONG).show();
+                }
+            }else{
+                Toast.makeText(this, "Ingrese la contraseña", Toast.LENGTH_LONG).show();
+            }
+        }else{
+            Toast.makeText(this, "Ingrese el Usuario", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public class clsAsincTask extends AsyncTask<clsLogin, Void, Void>{
+
+        @Override
+        protected Void doInBackground(clsLogin... params) {
+            return null;
+        }
+    }
+
 
 }
---
+++ app/src/main/java/co/ingenieria/sigma/util/clsConstantes.java
@@ -0,0 +1,10 @@
+package co.ingenieria.sigma.util;
+
+/**
+ * Created by John Agudelo on 08/02/2015.
+ */
+public class clsConstantes {
+    private static final String URL_BACKEND = "https//www.alumbradospublicos.co";
+    private static final String URL_DIR = URL_BACKEND+"/Aplicacion/herramientas/opraciones/";
+    private static final String URL_FACHADA = URL_DIR + "fachada.php";
+}
