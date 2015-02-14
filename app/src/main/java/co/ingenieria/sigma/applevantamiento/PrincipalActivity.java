0caf2abf30464e6f932e61bd2c7ad05cd39e6446
John Agudelo <johnaagudelo@gmail.com>
2015-02-14 18:52:23 -0500

    creacion activity transformador

--- app/src/main/java/co/ingenieria/sigma/applevantamiento/PrincipalActivity.java
+++ app/src/main/java/co/ingenieria/sigma/applevantamiento/PrincipalActivity.java
@@ -9,19 +9,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TabHost;
 import android.widget.TabHost.OnTabChangeListener;
+import android.widget.Toast;
 
 
 public class PrincipalActivity extends ActionBarActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tabs);
 
-        LocationManager locManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        WebView myWebView = (WebView) findViewById(R.id.viewpage);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://www.google.com");
 
         Resources res = getResources();
 
@@ -39,12 +47,11 @@ public class PrincipalActivity extends ActionBarActivity {
 
         tabs.setCurrentTab(0);
 
-        WebView myWebView = (WebView) findViewById(R.id.viewpage);
-        myWebView.getSettings().setJavaScriptEnabled(true);
-        myWebView.loadUrl("http://laofertaquequieres.co/Geomarketing/FUNLogin/PRELoginAutomatico?sistema=SIS_40&login=general&passwd=general");
+        LocationManager locManager = (LocationManager)getSystemService(LOCATION_SERVICE);
 
         if (!locManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
         {
+            Toast.makeText(this, "Para Continuar debe Activar el GPS", Toast.LENGTH_LONG).show();
             Intent settingsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
             settingsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
                     Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
@@ -52,6 +59,29 @@ public class PrincipalActivity extends ActionBarActivity {
         }
     }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        LocationManager locManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+        if (!locManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+        {
+            Toast.makeText(this, "Para Continuar debe Activar el GPS", Toast.LENGTH_LONG).show();
+
+            Intent login = new Intent(this, LoginActivity.class);
+            startActivity(login);
+        }
+    }
+
+    public void FrmApoyo(View view){
+        Intent abrirFrmApoyo = new Intent(this, ApoyoActivity.class);
+        startActivity(abrirFrmApoyo);
+    }
+
+    public void FrmTransformador(View view){
+        Intent abrirFrmTransformador = new Intent(this, TransformadorActivity.class);
+        startActivity(abrirFrmTransformador);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/co/ingenieria/sigma/util/clsServicio.java
@@ -0,0 +1,7 @@
+package co.ingenieria.sigma.util;
+
+/**
+ * Created by John Agudelo on 14/02/2015.
+ */
+public class clsServicio {
+}
